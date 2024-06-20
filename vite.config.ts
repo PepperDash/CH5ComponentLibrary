6429bcab950435f7d1d3f0dde89b95c8aa06c701
Andrew Welker <awelker@pepperdash.com>
2024-06-20 01:35:58 -0500

    fix: add types to package.json.exports

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import dts from 'vite-plugin-dts'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [ dts({include: ['src']})],
+  plugins: [ dts({include: ['src'], rollupTypes: true})],
   build: {
     copyPublicDir: false,
     lib: {
